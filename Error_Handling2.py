3c07f3577cfb9b716f530ca22761be477d18d1e4
saiuppalapati011 <157865454+saiuppalapati011@users.noreply.github.com>
2024-04-09 11:46:17 +0530

    Create Error_Handling2.py

---
+++ Error_Handling2.py
@@ -0,0 +1,8 @@
+try:
+  a=int(input("Enter a number:"));
+  bint(input("Enter another number:"))
+  prit(a+b)
+except(NameError):
+  print("Sorry for the Syntax Errors!")
+except(SyntaxError):
+  print("There is a Syntax error in the code.")
